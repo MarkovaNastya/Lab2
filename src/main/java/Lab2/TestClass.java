ef8c0c35763559b859c46ebfbcc34da0a2e41643
MarkovaNastya <tasenok_1999@mail.ru>
2019-10-29 23:51:42 +0300

    Scripted auto-commit on change (2019-10-29 23:51:42) by gitwatch.sh

--- src/main/java/Lab2/TestClass.java
+++ src/main/java/Lab2/TestClass.java
@@ -1,11 +1,10 @@
-//package Lab2;
-//
-//public class TestClass {
-//
-//    public static void main(String[] args) {
-//        String s ="\"10001\",\"Afognak Lake, AK: Afognak Lake Airport\"";
-//        //String s = "2015,1,1,4,7,2015-01-04,\"AA\",19805,\"AA\",\"N367AA\",\"7\",11298,1129803,30194,13830,\"2050\",\"2056\",261.00,261.00,0.00,\"\",464.00,3711.00,";
-//        ReaderCSV readerCSV = new ReaderCSV();
-//        readerCSV.parsLine(s);
-//    }
-//}
+package Lab2;
+
+public class TestClass {
+
+    public static void main(String[] args) {
+        String s = "";
+        float a = Float.parseFloat(s);
+        System.out.println(a);
+    }
+}
