fc00aed6fa7e4e5b480dd3f1fa80077f1e04f271
MarkovaNastya <tasenok_1999@mail.ru>
2019-10-29 19:23:47 +0300

    Scripted auto-commit on change (2019-10-29 19:23:47) by gitwatch.sh

--- src/main/java/Lab2/Main.java
+++ src/main/java/Lab2/Main.java
@@ -1,7 +1,25 @@
 package Lab2;
 
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+
 public class Main {
-    public static void main(String args[]){
+    public static void main(String args[]) {
+
+        Job job = Job.getInstance();
+        job.setJarByClass(JoinJob.class);
+        job.setJobName("JoinJob sort");
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, CallsJoinMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, SystemsJoinMapper.class);
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        job.setPartitionerClass(TextPair.FirstPartitioner.class);
+        job.setGroupingComparatorClass(TextPair.FirstComparator.class);
+        job.setReducerClass(JoinReducer.class);
+        job.setMapOutputKeyClass(TextPair.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        job.setNumReduceTasks(2);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
 
     }
 }
