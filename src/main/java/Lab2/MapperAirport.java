bb8e4716158e360b46dc7dbc7a592d5458d8630d
MarkovaNastya <tasenok_1999@mail.ru>
2019-10-29 15:12:44 +0300

    Scripted auto-commit on change (2019-10-29 15:12:44) by gitwatch.sh

--- src/main/java/Lab2/MapperAirport.java
+++ src/main/java/Lab2/MapperAirport.java
@@ -1,4 +1,10 @@
 package Lab2;
 
-public class Mapper {
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+
+public class MapperAirport extends Mapper<LongWritable, Text, AirportID, Text> {
+
+
 }
