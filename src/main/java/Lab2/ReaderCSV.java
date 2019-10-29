3b6e5f521622ee3283fc19f47e7c1c7c1ae98ae2
MarkovaNastya <tasenok_1999@mail.ru>
2019-10-29 20:48:37 +0300

    Scripted auto-commit on change (2019-10-29 20:48:37) by gitwatch.sh

--- src/main/java/Lab2/ReaderCSV.java
+++ src/main/java/Lab2/ReaderCSV.java
@@ -1,8 +1,27 @@
 package Lab2;
 
+import org.apache.hadoop.io.Text;
+
+import java.awt.*;
+import java.util.ArrayList;
+
 public class ReaderCSV {
 
-
     
+
+    private ArrayList<String> columns;
+
+    public ReaderCSV(){
+        columns=new ArrayList<>();
+    }
+
+    public void parsLine(Text t){
+        String line = t.toString();
+        for (int i = 0; i < line.length(); i++){
+            if
+        }
+    }
+
+
 
 }
