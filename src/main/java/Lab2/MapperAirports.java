644791fed8dbb2c825f5787e392b027fb0ec323f
MarkovaNastya <tasenok_1999@mail.ru>
2019-10-29 23:53:40 +0300

    Scripted auto-commit on change (2019-10-29 23:53:40) by gitwatch.sh

--- src/main/java/Lab2/MapperAirports.java
+++ src/main/java/Lab2/MapperAirports.java
@@ -10,7 +10,7 @@ public class MapperAirports extends Mapper<LongWritable, Text, KeyValue, Text> {
     private static final int AIRPORT_ID = 0;
     private static final int AIRPORT_NAME = 1;
     private static final String AIRPORT_FLAG = "0";
-    private static final String TITLE="Code,Description";
+    private static final String TITLE = "Code,Description";
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
