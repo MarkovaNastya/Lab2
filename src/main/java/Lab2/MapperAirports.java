5d1d31bc3556d2f58adeb8a624440000929b20b0
MarkovaNastya <tasenok_1999@mail.ru>
2019-10-29 21:54:47 +0300

    Scripted auto-commit on change (2019-10-29 21:54:47) by gitwatch.sh

--- src/main/java/Lab2/MapperAirports.java
+++ src/main/java/Lab2/MapperAirports.java
@@ -1,5 +1,15 @@
 package Lab2;
 
-public class MapperAirports {
-    
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class MapperAirports extends Mapper<LongWritable, Text, TextPair, Text> {
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        super.map(key, value, context);
+    }
 }
