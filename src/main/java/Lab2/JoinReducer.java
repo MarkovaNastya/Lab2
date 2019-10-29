ffd3db47a2d155e40ef940e96c320a3e245be040
MarkovaNastya <tasenok_1999@mail.ru>
2019-10-29 23:13:44 +0300

    Scripted auto-commit on change (2019-10-29 23:13:44) by gitwatch.sh

--- src/main/java/Lab2/JoinReducer.java
+++ src/main/java/Lab2/JoinReducer.java
@@ -1,4 +1,44 @@
 package Lab2;
 
-public class JoinReducer {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+public class JoinReducer extends Reducer<KeyValue, Text, Text, Text> {
+
+    @Override
+    protected void reduce(KeyValue key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+
+        Iterator<Text> iter = values.iterator();
+        
+//        Text systemInfo = new Text(iter.next());
+
+        String airportName = iter.next().toString();
+
+        float max = Float.parseFloat(iter.next().toString());
+        float min = max;
+        float average = max;
+        int count = 1;
+
+
+        while (iter.hasNext()) {
+
+            float delay = Float.parseFloat(iter.next().toString());
+
+            if (delay > max) {
+                max = delay;
+            }
+
+            if (delay < min) {
+                min = delay;
+            }
+
+            average += delay;
+            count++;
+        }
+        average /= count;
+
+    }
 }
