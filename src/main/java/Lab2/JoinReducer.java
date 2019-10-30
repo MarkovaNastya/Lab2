bc953949d77c336b5e0cee35dd30ff5a75800774
MarkovaNastya <tasenok_1999@mail.ru>
2019-10-30 12:45:28 +0300

    Scripted auto-commit on change (2019-10-30 12:45:28) by gitwatch.sh

--- src/main/java/Lab2/JoinReducer.java
+++ src/main/java/Lab2/JoinReducer.java
@@ -10,12 +10,12 @@ import java.util.Iterator;
 public class JoinReducer extends Reducer<KeyValue, Text, Text, Text> {
 
     private static final String SPACE = " ";
-    private static final int AIRPORT_NAME_LENGTH=75;
-    private static final int DELAY_LENGTH=15;
+    private static final int AIRPORT_NAME_LENGTH = 75;
+    private static final int DELAY_LENGTH = 15;
 
-    private String formatString(int len, String s){
-        for (int i=s.length(); i<len;i++){
-            s+=SPACE;
+    private String formatString(int len, String s) {
+        for (int i = s.length(); i < len; i++) {
+            s += SPACE;
         }
         return s;
     }
@@ -28,7 +28,7 @@ public class JoinReducer extends Reducer<KeyValue, Text, Text, Text> {
 
         String airportName = iter.next().toString();
 
-        if (iter.hasNext()){
+        if (iter.hasNext()) {
             float max = Float.parseFloat(iter.next().toString());
             float min = max;
             float average = max;
@@ -53,9 +53,12 @@ public class JoinReducer extends Reducer<KeyValue, Text, Text, Text> {
 
             average /= count;
 
-            Text outText = new Text(formatString(AIRPORT_NAME_LENGTH, airportName) + "Average delay time: " + formatString(DELAY_LENGTH, String.valueOf(average)) + "Maximum delay time: "+ formatString(DELAY_LENGTH, String.valueOf(max))+"Minimum delay time: "+formatString(DELAY_LENGTH, String.valueOf(min)));
+            Text outText = new Text(formatString(AIRPORT_NAME_LENGTH, airportName)
+                    + "Average delay time: " + formatString(DELAY_LENGTH, String.valueOf(average))
+                    + "Maximum delay time: " + formatString(DELAY_LENGTH, String.valueOf(max))
+                    + "Minimum delay time: " + formatString(DELAY_LENGTH, String.valueOf(min)));
 
-            Text outKey = new Text (String.valueOf(key.getKey()));
+            Text outKey = new Text(String.valueOf(key.getKey()));
 
             context.write(outKey, outText);
 
